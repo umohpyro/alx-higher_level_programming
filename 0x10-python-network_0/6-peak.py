4bcfed5bb0bbda3e9f48c4482320e9176d55c50a
Umoh Andem <andemumoh@gmail.com>
2023-01-16 15:47:00 -0800

    hot fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,15 +5,20 @@
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
 
-    if not list_of_integers:
+    if list_of_integers is None or list_of_integers == []:
         return None
-    n = len(list_of_integers)    
-    if n < 3:
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
         return max(list_of_integers)
-    mid = n // 2
-    if list_of_integers[mid] > list_of_integers[mid-1] and list_of_integers[mid] > list_of_integers[mid+1]:
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
-    else:
-        left_peak = find_peak(list_of_integers[:mid])
-        right_peak = find_peak(list_of_integers[mid:])
-        return max(left_peak, right_peak)
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return max(list_of_integers[mid], find_peak(list_of_integers[mid:]))
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return max(list_of_integers[mid], find_peak(list_of_integers[:mid]))
