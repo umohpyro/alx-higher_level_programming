ac8fc98d7e3fb30aeeeb9e28006493aff8258efe
Umoh Andem <andemumoh@gmail.com>
2023-01-17 05:24:26 -0800

    function fixed to produced exact result

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+    response = requests.post(url, payload)
+    print(response.text)
